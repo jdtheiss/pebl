cba4418d67fa9e2587dedcc1758592be06ae06a2
jdtheiss <justin.d.theiss@vanderbilt.edu>
2015-12-10 10:07:13 -0600

    Important Updates
    Fixed bug that caused chosen environments to be overwritten.

--- sawa/main/functions/sawa_editor.m
+++ sawa/main/functions/sawa_editor.m
@@ -67,7 +67,7 @@ if exist('envvar','var'),
 for x = 1:numel(envvar), 
 if ~isempty(envvar{x}) % setenv
 if ~any(strfind(getenv(envvar{x}),newpath{x})),
-if any(strfind(newpath{x}),filesep), newpath{x} = [':' newpath{x}]; end;
+if any(strfind(newpath{x},filesep)), newpath{x} = [':' newpath{x}]; end;
 setenv(envvar{x},[getenv(envvar{x}) newpath{x}]);
 end;
 else % addpath
