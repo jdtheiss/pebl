dd505cae081a25376e3ace4da806ed2edc607849
Justin Theiss <jdeantheiss@gmail.com>
2017-08-08 23:04:36 -0700

    Update to reflect change in pebl_setbatch function

--- pebl/functions/pebl_feval.m
+++ pebl/functions/pebl_feval.m
@@ -489,10 +489,6 @@ function [matlabbatch, dep] = local_setbatch(matlabbatch, options)
         if isa(options{x+1}, 'function_handle') && strncmp(func2str(options{x+1}), '@()', 3), 
             continue; 
         end
-        % if char file/dir, set cell
-        if ischar(options{x+1}) && (exist(options{x+1},'file') || isdir(options{x+1})),
-            options{x+1} = options(x+1);
-        end
         % switch based on first option in pair
         switch class(options{x})
             case 'struct' % use pebl_setfield with S
