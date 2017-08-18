cff4af6f42c7bfeae3abbf3c0554e15c8733dc6c
Justin Theiss <jdeantheiss@gmail.com>
2017-08-17 22:17:16 -0700

    Updated to provide greater flexibility with regard to 'end' usage

--- pebl/functions/pebl_setfield.m
+++ pebl/functions/pebl_setfield.m
@@ -80,11 +80,11 @@ if ~exist('remove','var'), remove = false; end;
 
 % set (:end)/{:end} to number of previous struct
 for n = find(~cellfun('isempty',R)),
-    R1 = regexprep(R{n}, '\{?\(?\[?\d+:end\]?\)?\}?.*', '');
+    R1 = regexprep(R{n}, '\{?\(?\[?\d*:?end\]?\)?\}?.*', '');
     if strcmp(R1, R{n}), continue; end;
     if isempty(R1), R1 = '(:)'; end;
     e = numel(subsref(A, sub2str(R1)));
-    R{n} = regexprep(R{n}, ':end', sprintf(':%d', e));
+    R{n} = regexprep(R{n}, '(:?)end', sprintf('$1%d', e));
 end
 
 % append S with R/append
