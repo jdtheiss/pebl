dd77d11acfd346a7fa986c25c295a0c6987074d4
Justin Theiss <jdeantheiss@gmail.com>
2017-08-17 21:37:16 -0700

    Updated for clarity and simplicity

--- pebl/functions/struct2gui.m
+++ pebl/functions/struct2gui.m
@@ -3,15 +3,21 @@ function [output, s] = struct2gui(s, varargin)
 % Create a gui from fields set in a structure array.
 %
 % Inputs:
-% s: structure, fields corresponding to uicontrols to set (i.e., each field
-% could be set as uicontrol(s.(field{n})))
-% 'wait': (optional), true/false to implement wait block until gui is
+% s: structure with fields of uicontrols to set (i.e., uicontrol(s.(field{n})))
+% optional structure fields:
+% 'order' - order of location in figure [1,1] to [column, row]
+%   [default is in order of fieldnames]
+% 'size' - size of uicontrol [width, height]
+%   [default figure size / (1.5 * number of columns/rows)]
+% 'spacing' - spacing between uicontrols/edge of figure
+%   [default .2 * figure size / number of columns/rows] 
+% optional additional arguments:
+% 'wait': boolean to implement wait block until gui is
 %   closed
 %   [default true]
-% 'data': (optional), any value to initiate for figure guidata
+% 'data': any value to initiate for figure guidata
 %   [defualt []]
-% 'done_button': (optional), true/false to include a done button in bottom
-%   right corner 
+% 'done_button': boolean to include a done button in bottom right corner 
 %   [default true]
 %
 % Outputs:
@@ -22,19 +28,22 @@ function [output, s] = struct2gui(s, varargin)
 % corresponding to uicontrol options (.e.g, s.push(1).string = 'button1').
 % In order to make setting the position easier, an 'order' field can be set
 % that will correspond to the relative position of an object where [1,1] is
-% top left and [2,8] is bottom right. Additionally, a 'size' field can be
-% set for a specific uicontrol in order to only change size but not
-% location. Finally, the default callback is to set the properties of the
-% uicontrol to the guidata in a structure field corresponding to the
-% uicontrols tag (default is style_number).
+% top left and [2,8] (default) is bottom right. If any orders exceed [2,8]
+% then the number of columns/rows will be updated.
+% Additionally, a 'size' field can be set for a specific uicontrol in order 
+% to only change size but not location. 
+% Finally, the default callback is to set the properties of the uicontrol 
+% to the guidata in a structure field corresponding to the uicontrols tag 
+% (default is style_number).
 %
 % Example: Change 'edit' to 'test' in gui then press 'btn1'.
 % s.push(1:4) = struct('order',{[1,3],[1,4],[2,3],[2,4]},'string',...
 %   {'btn1','btn2','btn3','btn4'});
 % s.push(1).callback = @(x,y)disp(getfield(getfield(guidata(gcf),'edit'),'String'));
-% s.edit = struct('order', [1,1], 'string', 'edit');
+% s.edit = struct('order', [1,2], 'string', 'edit');
 % [output, s] = struct2gui(s)
 % test
+% 
 % output = 
 % 
 %     edit: [1x1 struct]
@@ -64,11 +73,11 @@ set(0,'defaultTextFontSize',12);
 
 % init figure as hidden
 f = figure('visible','off','numbertitle','off','menubar','none',...
-    'resize','off','units','pixels','tag',mfilename);
+           'resize','off','units','pixels','tag',mfilename);
 
 % set position field
-if ~isfield(s, 'position'),
-    s.position = [360,500,450,300];
+if ~isfield(s, 'position') || isempty(s.position),
+    s.position = [360, 500, 450, 300];
 end
 
 % set done button
@@ -76,12 +85,25 @@ if done_button,
     btn_pos(1:2) = s.position(3:4) .* [.85, .01];
     btn_pos(3:4) = s.position(3:4) .* [.15, .1];
     done_btn = uicontrol(f, 'style', 'pushbutton', 'string', 'done',...
-        'position', btn_pos,'callback', 'set(gcf,''visible'',''off'');');
+                         'position', btn_pos, 'callback',...
+                         'set(gcf,''visible'',''off'');');
 end
 
-% get fields then set uicontrols
-s = local_setdefaults(s); 
-f = local_setfields(f, s);
+% set positions
+positions = local_setpositions(s);
+s = local_setdefaults(s, 'position', positions);
+
+% set tags
+[~, sub_tag] = pebl_getfield(s, 'expr', '^\.[^\.]+');
+tags = cellfun(@(x){regexprep(sub2str(x),'\W','')}, sub_tag);
+s = local_setdefaults(s, 'tag', tags);
+
+% set callbacks
+callback = @(x,y)guidata(gcf, setfield(guidata(gcf), get(x, 'tag'), get(x)));
+s = local_setdefaults(s, 'callback', {callback});
+
+% set uicontrols
+f = local_setfields(f, s, {'order','spacing','size'});
 
 % set data to gcf
 guidata(f, data);
@@ -100,88 +122,101 @@ else % otherwise make visible
     set(f, 'visible', 'on');
     output = [];
 end
+
+% reset fonts
+set(0,'defaultUicontrolFontSize',defUfont); 
+set(0,'defaultAxesFontSize',defAfont);
+set(0,'defaultTextFontSize',defTfont); 
 end
 
-function s = local_setdefaults(s)
-% get fields and values to be input to local_setfields
+function positions = local_setpositions(s)
 
-    % init position
-    if ~isfield(s, 'position') || isempty(s.position),
-        s.position = [360,500,450,300]; 
-    end
-    % get n of uicontrols and needed columns
-    uictrls = pebl_getfield(s,'fun', @(x)isa(x,'struct'), 'r', 1);
-    n = sum(cellfun(@(x)numel(x), uictrls));
-    % set rows and columns;
-    r = 8; c = max(2, ceil(n / r));
-    % init spacing and size
-    if ~isfield(s, 'spacing') || isempty(s.spacing),
-        spacing = [.2, .2] .* (s.position(3:4) ./ [c, r]);
-    else
-        spacing = s.spacing;
-    end
-    default_size = s.position(3:4) ./ (1.5 * [c, r]);
-    % create location function using default size
-    loc_fun = @(x)[spacing(1) + (spacing(1) + default_size(1)) * (x(1)-1),...
-        s.position(4) - (spacing(2) + default_size(2)) * x(2)];
-    % get fields without position and set to 0s
-    [~, sub_pos] = pebl_getfield(s, 'expr', '^\.[^\.]+', 'fun',...
-        @(x)isstruct(x)&&~isfield(x,'position')||isempty(x.position));
-    s = pebl_setfield(s, 'S', sub_pos, 'append', '.position', 'C', zeros(1,4)); 
-    % init default order
-    default_order = cat(2,sort(repmat([1:c]',r,1)),repmat([1:r]',c,1));
-    % get orders
-    [orders, sub_order] = pebl_getfield(s, 'expr', '.*\.order$', 'fun', @(x)~isempty(x));
-    set = cellfun(@(x)find(arrayfun(@(y)all(default_order(y,:)==x),...
-        1:size(default_order,1))), orders);
-    default_order = arrayfun(@(x){default_order(x,:)}, 1:size(default_order,1));
-    % set locations for orders
-    locs = cellfun(@(x){loc_fun(x)}, default_order);
-    % set position locations
-    [s, sub_order] = pebl_setfield(s, 'S', sub_order, 'remove', true); 
-    set_locs = locs(set); 
-    if numel(set_locs) == 1, set_locs = set_locs{1}; end;
-    s = pebl_setfield(s, 'S', sub_order, 'append', '.position(1:2)', 'C', set_locs);
-    locs(set) = [];  
-    % set unset locations
-    if ~isempty(locs), 
-        [~, sub_pos] = pebl_getfield(s, 'expr', '.*\.position$', 'fun', @(x)all(x(1:2)==0));
-        unset_locs = locs(1:numel(sub_pos)); 
-        if numel(unset_locs) == 1, unset_locs = unset_locs{1}; end;
-        s = pebl_setfield(s, 'S', sub_pos, 'append', '(1:2)', 'C', unset_locs);
-    end
-    % set position sizes
-    [sizes, sub_size] = pebl_getfield(s, 'expr', '.*\.size$');
-    [s, sub_size] = pebl_setfield(s, 'S', sub_size, 'remove', true);
-    if numel(sizes) == 1, sizes = sizes{1}; end;
-    s = pebl_setfield(s, 'S', sub_size, 'append', '.position(3:4)', 'C', sizes);
-    % set unset sizes
-    [~, sub_size] = pebl_getfield(s, 'expr', '.*\.position$', 'fun', @(x)all(x(3:4)==0));
-    s = pebl_setfield(s, 'S', sub_size, 'append', '(3:4)', 'C', default_size);
-    % set tags as style_number
-    [~, sub_tag] = pebl_getfield(s, 'expr', '^\.[^\.]+', 'fun',... 
-        @(x)isstruct(x)&&~isfield(x,'tag')||isempty(x.tag));
-    tags = cellfun(@(x){regexprep(sub2str(x),'[\.\(\)\{\}]','')}, sub_tag);
-    if numel(tags) == 1, tags = tags{1}; end;
-    s = pebl_setfield(s, 'S', sub_tag, 'append', '.tag', 'C', tags);
-    % set get(x) as callback
-    callback = @(x,y)guidata(gcf, setfield(guidata(gcf), regexprep(get(x,'tag'),'\W',''), get(x)));
-    [~, sub_cb] = pebl_getfield(s, 'expr', '^\.[^\.]+', 'fun',...
-        @(x)isstruct(x)&&~isfield(x, 'callback')||isempty(x.callback));
-    s = pebl_setfield(s, 'S', sub_cb, 'append', '.callback', 'C', callback);
+% get number of uicontrols
+n_ctrl = sum(cellfun(@(x)isstruct(x) * numel(x), struct2cell(s)));
+
+% get orders to determine columns/rows
+tmp = pebl_getfield(s, 'expr', '.*\.order');
+sz = max(cat(1, tmp{:}), [], 1);
+
+% set rows and columns;
+r = 8; c = max(2, ceil(n_ctrl / r));
+if ~isempty(sz) % set to max of orders or rows/columns
+    c = max(c, sz(1)); r = max(r, sz(2));
 end
 
-function f = local_setfields(f, s)
-% set uicontrols for f with fields, values, defaults
+% set default order
+default_order = num2cell(cat(2,sort(repmat([1:c]',r,1)),repmat([1:r]',c,1)), 2);
 
-fields = fieldnames(s);
-for n = 1:numel(fields), 
-    if ~isstruct(s.(fields{n})), % main fields
-        set(f, fields{n}, s.(fields{n}));
-    elseif ~isempty(s.(fields{n})), % uicontrol
-        for m = 1:numel(s.(fields{n})),
-            s.(fields{n})(m).style = fields{n}; 
-            uicontrol(f, s.(fields{n})(m));
+% set default spacing
+default_spacing = [.2, .2] .* (s.position(3:4) ./ [c, r]);
+
+% set default size
+default_size = s.position(3:4) ./ (1.5 * [c, r]);
+
+% set defaults
+s = local_setdefaults(s, 'order', default_order);
+s = local_setdefaults(s, 'spacing', {default_spacing});
+s = local_setdefaults(s, 'size', {default_size});
+
+% get orders, spacings, and sizes
+orders = pebl_getfield(s, 'expr', '.*\.order');
+spacings = pebl_getfield(s, 'expr', '.*\.spacing');
+sizes = pebl_getfield(s, 'expr', '.*\.size');
+
+% if not found, return empty cell
+if isempty(orders) || isempty(spacings) || isempty(sizes),
+    positions = {};
+    return;
+end
+
+% calculate locations
+positions = cellfun(@(x,y,z){[(x(1)-1) * (y(1) + z(1)), x(2) * (y(2) + z(2)), z]},...
+                    orders, spacings, repmat({default_size}, size(orders)));
+                
+% update locations based on spacing/figure size; append sizes 
+positions = cellfun(@(x,y,z){[x(1) + y(1), s.position(4) - y(2), z]},...
+                    spacings, positions, sizes);
+end
+
+function s = local_setdefaults(s, field, value)
+
+% set index counter
+i = 1;
+% get each struct of s
+c = struct2cell(s);
+% for each set value
+for n = 1:numel(c),
+    if isstruct(c{n}),
+        for m = 1:numel(c{n}),
+            if ~isfield(c{n}(m), field) || isempty(c{n}(m).(field)),
+                c{n}(m).(field) = value{min(end, i)};
+                i = i + 1;
+            end
+        end
+    end
+end
+% set output
+s = cell2struct(c, fieldnames(s));
+end
+
+function f = local_setfields(f, s, rmflds)
+
+% remove fields and get properties
+s = rmfield(s, rmflds(isfield(s, rmflds)));
+props = fieldnames(s)';
+
+% for each property, set 
+for n = 1:numel(props), 
+    % if struct, set figure
+    if ~isstruct(s.(props{n})), 
+        set(f, props{n}, s.(props{n}));
+    elseif ~isempty(s.(props{n})), % uicontrol
+        % remove fields
+        s.(props{n}) = rmfield(s.(props{n}), rmflds(isfield(s.(props{n}), rmflds)));
+        % for each component, set uicontrol
+        for m = 1:numel(s.(props{n})),
+            s.(props{n})(m).style = props{n}; 
+            uicontrol(f, s.(props{n})(m));
         end
     end
 end
